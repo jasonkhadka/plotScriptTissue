2243458c663b5ab33384d81394a17ce32d70c17f
jason khadka <jasonkhadka33@gmail.com>
2018-12-10 14:39:14 +0100

    added cloud on scatter points in anisotropy plot

--- strain_plots/plotAnistropyHeightGrowthScatter.py
+++ strain_plots/plotAnistropyHeightGrowthScatter.py
@@ -25,98 +25,19 @@ plt.rcParams['axes.titlesize'] = 22
 
 import numpy as n, pylab as p, time
 import scipy.interpolate as interpolate
-
-def _angle_to_point(point, centre):
-	'''calculate angle in 2-D between points and x axis'''
-	delta = point - centre
-	res = n.arctan(delta[1] / delta[0])
-	if delta[0] < 0:
-		res += n.pi
-	return res
-
-def _draw_triangle(p1, p2, p3, **kwargs):
-	tmp = n.vstack((p1,p2,p3))
-	x,y = [x[0] for x in zip(tmp.transpose())]
-	p.fill(x,y, **kwargs)
-
-def area_of_triangle(p1, p2, p3):
-	'''calculate area of any triangle given co-ordinates of the corners'''
-	return n.linalg.norm(n.cross((p2 - p1), (p3 - p1)))/2.
-
-
-def convex_hull(points, graphic=False, smidgen=0.0075,scale = 1.05):
-	'''
-	Calculate subset of points that make a convex hull around points
-	Recursively eliminates points that lie inside two neighbouring points until only convex hull is remaining.
-
-	:Parameters:
-	points : ndarray (2 x m)
-	array of points for which to find hull
-	graphic : bool
-	use pylab to show progress?
-	smidgen : float
-	offset for graphic number labels - useful values depend on your data range
-
-	:Returns:
-	hull_points : ndarray (2 x n)
-	convex hull surrounding points
-	'''
-
-	if graphic:
-		p.clf()
-		p.plot(points[0], points[1], 'ro')
-	n_pts = points.shape[1]
-	assert(n_pts > 5)
-	centre = points.mean(1)
-	if graphic: p.plot((centre[0],),(centre[1],),'bo')
-	angles = n.apply_along_axis(_angle_to_point, 0, points, centre)
-	pts_ord = points[:,angles.argsort()]
-	if graphic:
-		for i in xrange(n_pts):
-			p.text(pts_ord[0,i] + smidgen, pts_ord[1,i] + smidgen, \
-				   '%d' % i)
-	pts = [x[0] for x in zip(pts_ord.transpose())]
-	prev_pts = len(pts) + 1
-	k = 0
-	while prev_pts > n_pts:
-		prev_pts = n_pts
-		n_pts = len(pts)
-		if graphic: p.gca().patches = []
-		i = -2
-		while i < (n_pts - 2):
-			Aij = area_of_triangle(centre, pts[i],     pts[(i + 1) % n_pts])
-			Ajk = area_of_triangle(centre, pts[(i + 1) % n_pts], \
-								   pts[(i + 2) % n_pts])
-			Aik = area_of_triangle(centre, pts[i],     pts[(i + 2) % n_pts])
-			if graphic:
-				_draw_triangle(centre, pts[i], pts[(i + 1) % n_pts], \
-							   facecolor='blue', alpha = 0.2)
-				_draw_triangle(centre, pts[(i + 1) % n_pts], \
-							   pts[(i + 2) % n_pts], \
-							   facecolor='green', alpha = 0.2)
-				_draw_triangle(centre, pts[i], pts[(i + 2) % n_pts], \
-							   facecolor='red', alpha = 0.2)
-			if Aij + Ajk < Aik:
-				if graphic: p.plot((pts[i + 1][0],),(pts[i + 1][1],),'go')
-				del pts[i+1]
-			i += 1
-			n_pts = len(pts)
-		k += 1
-	return scale*n.asarray(pts)
+from scipy.spatial import ConvexHull
 ###############################################################################################################
 # to interpolate the data
 ###############################################################################################################
 def interpolatedata(data):
-	x = data[:,0]
-	y = data[:,1]
-	t = np.arange(x.shape[0], dtype=float)
-	t /= t[-1]
-	nt = np.linspace(0, 1, 100)
-	t /= t[-1]
-	x2 = interpolate.spline(t, x, nt)
-	y2 = interpolate.spline(t, y, nt)
-	return x2,y2
-
+    x = data[0,:]
+    y = data[1,:]
+    t = np.arange(x.shape[0], dtype=float)
+    nt = np.linspace(0, 1, 100)
+    t /= t[-1]
+    x2 = interpolate.spline(t, x, nt)
+    y2 = interpolate.spline(t, y, nt)
+    return x2,y2
 ###############################################################################################################
 # Get 2 dictionaries one with radialDirection vectors for each face and 
 # another with orthoradialDirection vectors for each face
@@ -426,9 +347,9 @@ def plotHeightGrowthScatter(numOfLayer, targetid,endStep,eta,
 		print tissueSurfaceArea, tissueSurfaceArea2,dTissueSurfaceArea, step , step2
 	########################################################################
 	if cloudCondition:
-		points = np.vstack((meanstressdiffarray,areadiffarray))
-		hull_pts = convex_hull(points)
-		hull_pts = np.vstack((hull_pts,hull_pts[0]))
+		points = np.vstack((meanstressdiffarray,areadiffarray)).T
+		hull_pts = ConvexHull(points)
+		hull_pts = points[hull_pts.vertices].T
 		interpolatex, interpolatey = interpolatedata(hull_pts)
 		anisotropyplot.plot(interpolatex, interpolatey, c = color,ls= '--',lw=2)
 	########################################################################
